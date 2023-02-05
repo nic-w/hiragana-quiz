70eb7736a089029f52b1accf59a6eec5152d66aa
Nic Webster <NicWebster96@gmail.com>
2023-02-04 22:35:32 -0800

    remove fade out feature

--- src/components/Response.tsx
+++ src/components/Response.tsx
@@ -15,11 +15,6 @@ const ResponseContainer = styled.div`
     props.response === "Correct!" || props.response === "Type and press Enter" ? "1em" : "2em"};
   color: ${(props) =>
     props.response === "Correct!" || props.response === "Type and press Enter" ? "white" : "red"};
-
-  // When user is wrong, have the correct romaji fade out
-  transition: opacity 7s;
-  opacity: ${(props) =>
-    props.response !== "Correct!" && props.response !== "Type and press Enter" ? 0 : 1};
 `;
 
 const Hiragana = ({ response }: { response: string }) => {
